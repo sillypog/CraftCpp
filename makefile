749a7b84b0ed8308a76527fc78b8bb6b00642e54
Peter Hastie <pete@sillypog.com>
2014-09-29 23:15:24 -0700

    Loading png files with libpng.

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 CFLAGS=-Wall -Wextra
 INCLUDE_PATHS = -I/Users/sillypog/OpenGL/glfw/include -I/Users/sillypog/OpenGL/glew/glew-1.10.0-built/include
 LIBRARY_PATHS = -L/Users/sillypog/OpenGL/glfw/build/src -L/Users/sillypog/OpenGL/glew/glew-1.10.0-built/lib
-LINKER_FLAGS = -lglew -lglfw3 -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo
+LINKER_FLAGS = -lglew -lglfw3 -lpng -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo
 
 all:
-	g++ $(CFLAGS) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(LINKER_FLAGS) src/main.cpp -o GLFWWindowCpp
+	g++ -v $(CFLAGS) $(INCLUDE_PATHS) $(LIBRARY_PATHS) $(LINKER_FLAGS) src/main.cpp -o GLFWWindowCpp
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <png.h>
 
 #include <cmath>	// Needed for sin
 #include <fstream>	// Needed to read files
@@ -46,8 +47,10 @@ float pixels[] = {
 string vertexShaderFile;
 string fragmentShaderFile;
 
+GLubyte *textureImage;
+
 void error_callback(int error, const char* description){
-	cout << "There was an error: " << description << endl;
+	cout << "There was an error: " << error << ": " << description << endl;
 }
 
 void loadShader(string filename, string &shader){
@@ -77,6 +80,123 @@ void checkShaderStatus(GLuint shader, string shaderName){
 		glGetShaderInfoLog(shader, 512, NULL, buffer);
 		cout << buffer;
 	}
+}
+
+bool loadPngImage(char *name, int &outWidth, int &outHeight, bool &outHasAlpha, GLubyte **outData) {
+    png_structp png_ptr;
+    png_infop info_ptr;
+    unsigned int sig_read = 0;
+    int color_type, interlace_type;
+    FILE *fp;
+
+    if ((fp = fopen(name, "rb")) == NULL)
+        return false;
+
+    /* Create and initialize the png_struct
+     * with the desired error handler
+     * functions.  If you want to use the
+     * default stderr and longjump method,
+     * you can supply NULL for the last
+     * three parameters.  We also supply the
+     * the compiler header file version, so
+     * that we know if the application
+     * was compiled with a compatible version
+     * of the library.  REQUIRED
+     */
+    png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
+                                     NULL, NULL, NULL);
+
+    if (png_ptr == NULL) {
+        fclose(fp);
+        return false;
+    }
+
+    /* Allocate/initialize the memory
+     * for image information.  REQUIRED. */
+    info_ptr = png_create_info_struct(png_ptr);
+    if (info_ptr == NULL) {
+        fclose(fp);
+        png_destroy_read_struct(&png_ptr, NULL, NULL);
+        return false;
+    }
+
+    /* Set error handling if you are
+     * using the setjmp/longjmp method
+     * (this is the normal method of
+     * doing things with libpng).
+     * REQUIRED unless you  set up
+     * your own error handlers in
+     * the png_create_read_struct()
+     * earlier.
+     */
+    if (setjmp(png_jmpbuf(png_ptr))) {
+        /* Free all of the memory associated
+         * with the png_ptr and info_ptr */
+        png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+        fclose(fp);
+        /* If we get here, we had a
+         * problem reading the file */
+        return false;
+    }
+
+    /* Set up the output control if
+     * you are using standard C streams */
+    png_init_io(png_ptr, fp);
+
+    /* If we have already
+     * read some of the signature */
+    png_set_sig_bytes(png_ptr, sig_read);
+
+    /*
+     * If you have enough memory to read
+     * in the entire image at once, and
+     * you need to specify only
+     * transforms that can be controlled
+     * with one of the PNG_TRANSFORM_*
+     * bits (this presently excludes
+     * dithering, filling, setting
+     * background, and doing gamma
+     * adjustment), then you can read the
+     * entire image (including pixels)
+     * into the info structure with this
+     * call
+     *
+     * PNG_TRANSFORM_STRIP_16 |
+     * PNG_TRANSFORM_PACKING  forces 8 bit
+     * PNG_TRANSFORM_EXPAND forces to
+     *  expand a palette into RGB
+     */
+    png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_16 | PNG_TRANSFORM_PACKING | PNG_TRANSFORM_EXPAND, NULL);
+
+    png_uint_32 width, height;
+    int bit_depth;
+    png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type,
+                 &interlace_type, NULL, NULL);
+    outWidth = width;
+    outHeight = height;
+    outHasAlpha = (color_type & PNG_COLOR_MASK_ALPHA);
+
+    unsigned int row_bytes = png_get_rowbytes(png_ptr, info_ptr);
+    *outData = (unsigned char*) malloc(row_bytes * outHeight);
+
+    png_bytepp row_pointers = png_get_rows(png_ptr, info_ptr);
+
+    for (int i = 0; i < outHeight; i++) {
+        // note that png is ordered top to
+        // bottom, but OpenGL expect it bottom to top
+        // so the order or swapped
+        memcpy(*outData+(row_bytes * (outHeight-1-i)), row_pointers[i], row_bytes);
+    }
+
+    /* Clean up after the read,
+     * and free any memory allocated */
+    png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+
+    /* Close the file */
+    fclose(fp);
+
+    /* That's it */
+    return true;
 }
 
 int main() {
@@ -110,6 +230,16 @@ int main() {
     printf("%u\n", vertexBuffer);*/
 
     printf("OpenGL version supported by this platform (%s): \n", glGetString(GL_VERSION));
+
+    // Load texture image
+    int width, height;
+    bool hasAlpha;
+    char filename[] = "textures/circle.png";
+    bool success = loadPngImage(filename, width, height, hasAlpha, &textureImage);
+    printf("Outcome of loading %s: %s \n ", filename, success ? "success" : "fail");
+    if (success){
+    	cout << filename << ": " << width << "x" << height << ", hasAlpha: " << hasAlpha << endl;
+    }
 
     // Prepare things for rendering
     GLuint vao;
