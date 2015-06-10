745cf25cd1f7c730618a3274307234e0acc20fc5
Peter Hastie <pete@sillypog.com>
2015-06-09 22:16:55 -0700

    Various image manipulations in the fragment shader.

--- shaders/quad_fragment_shader.glsl
+++ shaders/quad_fragment_shader.glsl
@@ -7,6 +7,22 @@ out vec4 outColor;
 uniform sampler2D texFramebuffer;
 
 void main(){
-	outColor = texture(texFramebuffer, Texcoord);
-	//outColor = vec4(0.0, 1.0, 0.0, 1.0);
+	// Invert the colors
+	outColor = vec4(1.0, 1.0, 1.0, 1.0) - texture(texFramebuffer, Texcoord);
+
+	// Greyscale
+	//outColor = texture(texFramebuffer, Texcoord);
+	//float avg = 0.2126 * outColor.r + 0.7152 * outColor.g + 0.0722 * outColor.b;
+	//outColor = vec4(avg, avg, avg, 1.0);
+
+	// Sobel edge detection
+	//vec4 s1 = texture(texFramebuffer, Texcoord - 1.0 /300.0 - 1.0 / 200.0);
+	//vec4 s2 = texture(texFramebuffer, Texcoord + 1.0 /300.0 - 1.0 / 200.0);
+	//vec4 s3 = texture(texFramebuffer, Texcoord - 1.0 /300.0 + 1.0 / 200.0);
+	//vec4 s4 = texture(texFramebuffer, Texcoord + 1.0 /300.0 + 1.0 / 200.0);
+	//vec4 sx = 4.0 * ((s4 + s3) - (s2 + s1));
+	//vec4 sy = 4.0 * ((s2 + s4) - (s1 + s3));
+	//vec4 sobel = sqrt(sx * sx + sy * sy);
+	//outColor = sobel;
+
 }
